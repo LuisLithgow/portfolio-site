5383afb8c1846db699e1d282a1dfb776685466fb
Luis Lithgow <lalithgow@gmail.com>
2016-06-16 08:00:12 -0400

    Started some JS.

---
+++ js/starter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+console.log('loaded');
+
+$(".img-circle").hover(function() {
+  $(".img-circle").addClass('animate bounceIn');
+  /* Stuff to do when the mouse enters the element */
+});
+
+
+
+
+
+
+})
