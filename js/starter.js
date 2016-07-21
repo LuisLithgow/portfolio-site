ebe715b09f8883541e1fa83407ec74b4a0809461
Luis Lithgow <lalithgow@gmail.com>
2016-07-20 23:05:59 -0400

    Testing parlax website

--- js/starter.js
+++ js/starter.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
 console.log('loaded');
 
-$(".img-circle").hover(function() {
-  $(".img-circle").addClass('animate bounceIn');
+// $(".img-circle").hover(function() {
+  // $(".img-circle").addClass('animate bounceIn');
   /* Stuff to do when the mouse enters the element */
-});
+// });
+
+  let parallax = document.querySelectorAll(".parallax"),
+      speed = 0.5;
+
+  window.onscroll = function(){
+    [].slice.call(parallax).forEach(function(el,i){
+
+      var windowYOffset = window.pageYOffset,
+          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+
+      el.style.backgroundPosition = elBackgrounPos;
+
+    });
+  };
 
 
 
