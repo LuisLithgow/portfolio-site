47e3a06ea9bd2a794a357b057e9a4df2e9fae568
Luis Lithgow <lalithgow@gmail.com>
2016-08-05 10:07:31 -0400

    Change colorway

--- js/starter.js
+++ js/starter.js
@@ -6,23 +6,42 @@ console.log('loaded');
   /* Stuff to do when the mouse enters the element */
 // });
 
-  let parallax = document.querySelectorAll(".parallax"),
-      speed = 0.5;
+  // let parallax = document.querySelectorAll(".parallax"),
+  //     speed = 0.5;
 
-  window.onscroll = function(){
-    [].slice.call(parallax).forEach(function(el,i){
+  // window.onscroll = function(){
+  //   [].slice.call(parallax).forEach(function(el,i){
 
-      var windowYOffset = window.pageYOffset,
-          elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+  //     var windowYOffset = window.pageYOffset,
+  //         elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
 
-      el.style.backgroundPosition = elBackgrounPos;
+  //     el.style.backgroundPosition = elBackgrounPos;
 
-    });
-  };
-
-
+  //   });
+  // };
 
 
 
+// function changeImage()
+// {
+//     let img = document.getElementById("img");
+//       img.src = images[x];
+//       x++;
 
-})
+//       if(x >= images.length){
+//           x = 0;
+//       }
+
+//     // fadeImg(img, 10, true);
+//     setTimeout(changeImage, 700);
+// }
+
+//   let images = [],
+//   x = 0;
+
+//   images[0] = "./img/me3.jpg";
+//   images[1] = "./img/me1.jpg";
+//   images[2] = "./img/me2.jpg";
+//   setTimeout(changeImage, 700);
+
+// })
